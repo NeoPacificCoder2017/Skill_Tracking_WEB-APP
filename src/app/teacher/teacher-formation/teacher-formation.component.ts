9efcbf759abb000f038a48bd9de2a378743ebbb5
Sheinehah <maui.sheinehah@gmail.com>
2018-07-17 09:11:54 -1000

    liste des étudiant appartenant à un formation

--- src/app/teacher/teacher-formation/teacher-formation.component.ts
+++ src/app/teacher/teacher-formation/teacher-formation.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api/api.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-teacher-formation',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teacher-formation.component.css']
 })
 export class TeacherFormationComponent implements OnInit {
+  listeEtudiant: any;
+  idEtudiant: number;
 
-  constructor() { }
+  constructor(private apiService: ApiService, private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.getEtudiants();
+  }
+
+  selectedEtudiant(idEtudiant) {
+    console.log('selectedEtudiant', idEtudiant);
+    this.router.navigate(['/student']);
+  }
+
+  public getEtudiants() {
+    this.apiService.get('students')
+    .subscribe((data) => {
+      console.log('data', data);
+      this.listeEtudiant = data.data;
+      console.log('students data', this.listeEtudiant);
+    });
   }
 
 }
