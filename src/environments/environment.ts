bb169942b0633483b010f603e858a59f8651c550
Teremu MARO <teremumaro@gmail.com>
2018-07-27 14:07:12 -1000

    Finalisation de la création d'une formation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://test.skillstracking.npc/api/',
-  appUrl: 'http://test.skillstracking.npc/',
-  appImageUrl: 'http://test.skillstracking.npc/uploads/images/',
-  appLogoUrl: 'http://test.skillstracking.npc/uploads/logos/',
-  appCalendarUrl: 'http://test.skillstracking.npc/uploads/calendars/',
+  apiUrl: 'http://skillstracking.motjo.io/api/',
+  appUrl: 'http://skillstracking.motjo.io/',
+  appImageUrl: 'http://skillstracking.motjo.io/uploads/images/',
+  appLogoUrl: 'http://skillstracking.motjo.io/uploads/logos/',
+  appCalendarUrl: 'http://skillstracking.motjo.io/uploads/calendars/',
 
   tableProgressRadiusTeacher: 30,
   tableProgressRadiusStudent: 24,
