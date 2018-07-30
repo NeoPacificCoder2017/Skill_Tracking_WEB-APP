6fa96ba99d3463631b9913f500dbc280ba4fccd3
unknown <gaby@1click.pf>
2018-07-30 08:40:29 +0200

    modal ajouter de l'jinterface html et methode create user ajouter

--- src/app/admin/admin-users/admin-users.component.ts
+++ src/app/admin/admin-users/admin-users.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../services/api/api.service';
 import { environment } from '../../../environments/environment';
 
@@ -11,10 +15,19 @@ export class AdminUsersComponent implements OnInit {
 
   environment = environment;
   users: any;
+  newUserForm: FormGroup;
+  loading = false;
+  submitted = false;
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.newUserForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', Validators.required],
+      profile_type_id: ['', Validators.required]
+  });
     this.apiService.get('users').subscribe(
       data => {
         console.log('data', data);
@@ -23,4 +36,32 @@ export class AdminUsersComponent implements OnInit {
     );
   }
 
+  get f() { return this.newUserForm.controls; }
+
+  createUser(): any {
+    this.submitted = true;
+    this.loading = true;
+    const start_at = this.f.start_at.value.split('/');
+    const end_at = this.f.end_at.value.split('/');
+    const uploadData = new FormData();
+    uploadData.append('firstname', this.f.firstname.value);
+    uploadData.append('lastname', this.f.lastname.value);
+    uploadData.append('email', this.f.email.value);
+    uploadData.append('profile_type_id', this.f.profile_type_id.value);
+
+    console.log('uploadData', uploadData);
+    this.apiService.upload('user/create', uploadData)
+    .subscribe(data => {
+      const element: HTMLElement = document.getElementById('closeModal') as HTMLElement;
+      element.click();
+      this.ngOnInit();
+    });
+  }
+
+  deleteUser(idUser): any {
+    this.apiService.delete('user/' + idUser)
+    .subscribe(data => {
+      this.ngOnInit();
+    });
+  }
 }
