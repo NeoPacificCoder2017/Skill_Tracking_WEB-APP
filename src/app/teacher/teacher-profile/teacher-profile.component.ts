14763163b5af441ba0a7f2b8a20370effb3f4113
Sheinehah <maui.sheinehah@gmail.com>
2018-08-23 21:48:16 -1000

    Teacher Profile Page

--- src/app/teacher/teacher-profile/teacher-profile.component.ts
+++ src/app/teacher/teacher-profile/teacher-profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from './../../../environments/environment';
 import { Location } from '@angular/common';
+import { ApiService } from '../../services/api/api.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-teacher-profile',
@@ -9,19 +12,33 @@ import { Location } from '@angular/common';
 })
 export class TeacherProfileComponent implements OnInit {
 
+  listFormations: any;
   environment = environment;
   dataTeacher: any;
   public me: any;
+  tabSelected = 1;
 
-  constructor(private location: Location) { }
+  constructor(private location: Location,
+    private apiService: ApiService,
+    private router: Router,
+    private http: HttpClient) { }
 
   ngOnInit() {
     this.me = JSON.parse(localStorage.getItem('user'));
     console.log('this.me', this.me);
+    this.getFormations();
   }
 
   goBack() {
     this.location.back();
   }
 
+
+  public getFormations(): any {
+    this.apiService.get('teacher/myFormations')
+    .subscribe((data) => {
+      this.listFormations = data.data;
+      console.log('this.listFormations', this.listFormations);
+    });
+  }
 }
