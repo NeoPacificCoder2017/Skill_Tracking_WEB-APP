739327452f9084e115d27695659588865834ece5
Teremu MARO <teremumaro@gmail.com>
2018-09-04 09:05:21 -1000

    correction erreur

--- src/app/admin/admin-formation/admin-formation.component.ts
+++ src/app/admin/admin-formation/admin-formation.component.ts
@@ -180,7 +180,7 @@ export class AdminFormationComponent implements OnInit {
   selectColor(index) {
     console.log('this.newModuleForm.controls', this.newModuleForm.controls);
     this.selectedColor = this.colorsPanel[index];
-    this.newModuleForm.controls.color.value = this.selectedColor;
+    // this.newModuleForm.controls.color.value = this.selectedColor;
     this.showColorsPanel = 0;
     console.log('index', index);
     console.log('selectedColor', this.selectedColor);
